6fcba43b53cd6af47021cc8bc9c006ca71d15aaf
gabrielaggp <69159631+gabrielaggp@users.noreply.github.com>
2021-11-16 13:35:49 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+import './App.css';
+
+//Components
+import NavBar from './Components/Header/NavBar';
+import ItemListContainer from './Components/Header/ItemListContainer/ItemListContainer';
+
+
+class App extends Component {
+  render(){
+  return (
+    <div className="App">
+      <NavBar />
+      <ItemListContainer greeting='Bienvenido a Librería Claraboya' />
+    </div>
+    
+  );}
+}
+
+export default App;
